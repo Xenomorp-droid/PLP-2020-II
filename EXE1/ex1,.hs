2728cb2b6a3081e784aca67f91d347126fbd1fb1
Xenomorp-droid <73622921+Xenomorp-droid@users.noreply.github.com>
2020-10-28 22:39:56 -0300

    Add files via upload

---
+++ EXE1/ex1,.hs
@@ -0,0 +1,25 @@
+--1.Função para calcular o quadrado do dobro do seu argumento:
+
+--Código:
+quadradoDobro x = (x + x)^2
+
+--2.Função para calcular o dobro do quadrado do seu argumento:
+
+--Código:
+dobroQuadrado x = (x ^ 2)*2
+
+--3.Função que receba três notas de um aluno e calcule a média aritmética das notas:
+
+--Código:
+media n1 n2 n3 = (n1 + n2 + n3)/3
+
+--4.Função que receba o valor do salário mínimo e a quantidade de quilowatts..
+--consumida por uma residência, e resulta no valor a ser pago com desconto de 15%..
+--valor do quilowatt de energia elética custa um quinto do salário mínimo:
+
+--Código:
+contz salari qulowtz = y - x 
+where
+valor = salari / 5
+y = valor * qulowtz
+x = y * 0.15
